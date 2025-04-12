a36c142d6baee955037f0ced44247a0bb5a7d992
김상수 <151427064+raminkim@users.noreply.github.com>
2025-04-12 22:37:06 +0900

    Implementing Naver Weather News Crawling

--- LLM_Weather/naver_news_crawler.py
+++ LLM_Weather/naver_news_crawler.py
@@ -1,8 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 import config
-def get_naver_news_crawler(location="서울", display=10, start=1) -> dict:
+import re
+
+def get_naver_news_url(location="서울", display=10, start=1) -> dict:
     """
-    네이버 뉴스 검색 API를 이용해 뉴스 기사를 가져오는 함수
+    네이버 뉴스 검색 API를 이용해 네이버 뉴스 url를 가져오는 함수
 
     Args:
         location (str): 검색할 지역 (ex: "서울).
@@ -16,7 +19,6 @@ def get_naver_news_crawler(location="서울", display=10, start=1) -> dict:
     # 네이버 검색 API를 사용하기 위한 CLIENT_ID, CLIENT_SECRET
     NAVER_CLIENT_ID = config.NAVER_CLIENT_ID
     NAVER_CLIENT_SECRET = config.NAVER_CLIENT_SECRET
-
 
     # 기본 네이버 뉴스 검색 URL과 쿼리 파라미터, 헤더 설정
     request_url = f"https://openapi.naver.com/v1/search/news.json?"
@@ -41,12 +43,51 @@ def get_naver_news_crawler(location="서울", display=10, start=1) -> dict:
         raise Exception(f"NAVER 검색 API 호출 실패: {answer.status_code}, {answer.text}")
 
 
-if __name__ == "__main__":
+def get_naver_news_crawler(location = "서울") -> list:
+    """
+    get_naver_news_url 함수를 통해 받아온 네이버 뉴스 url을 크롤링하는 함수.
+
+    Args:
+        location (str): 검색할 지역 (ex: "서울).
+
+    Returns:
+        list: 한 줄 요약된 뉴스들을 담은 리스트.
+    """
+
     try:
-        naver_news_data = get_naver_news_crawler()
+        # TODO: flask로 요청 받은 값을 get_naver_news_url 함수에 인수 location으로 넘겨야 함.
+        naver_news_data = get_naver_news_url(location)
 
         for items in naver_news_data["items"]:
-            print(items["description"])
+            # BeautifulSoup 기본 세팅
+            headers = {"User-Agent": "Mozilla/5.0"}
+            link = items["link"]
+            request = requests.get(link, headers=headers)
+            soup = BeautifulSoup(request.text, "html.parser")
+
+            # 네이버 뉴스 기사 내용을 가져오기
+            article_content = soup.find(id = "dic_area")
+
+            # HTML 태그 형식 탐지해 지우기
+            clean_article_content = re.sub(r'<.*?>', '', str(article_content))
+            print(clean_article_content)
+            print('----')
 
     except Exception as e:
         print(e)
+
+def llm_summarize_news(article_content):
+    """
+    주어진 뉴스 기사 내용을 openAI API를 사용하여 한 줄로 요약합니다.
+
+    Args:
+        location (str): 검색할 지역 (ex: "서울).
+
+    Returns:
+        list: 한 줄 요약된 뉴스들을 담은 리스트.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    get_naver_news_crawler()
