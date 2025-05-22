23a53b5bca0a55e9867b4e1bfecb90558418dfca
hhhjin <kkp7125@gmail.com>
2025-05-22 17:40:04 +0900

    필요없는 거 삭제

--- LLM_Weather/db.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-from config import SQLALCHEMY_DATABASE_URI
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
